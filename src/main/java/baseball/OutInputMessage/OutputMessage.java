0a2ab25e8a7caadefdbd3dfca05cea28d3eba458
seungjoo1 <tmdwn1107@naver.com>
2023-10-24 13:44:59 +0900

    입력받은 수를 return 하기 위한 함수 기능 구현

--- src/main/java/baseball/OutInputMessage/OutputMessage.java
+++ src/main/java/baseball/OutInputMessage/OutputMessage.java
@@ -4,16 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 
 public class OutputMessage {
-    public void StartGame(){
+    final public void StartGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
-    public void RequestInputNumber(){
+    final public void RequestInputNumber(){
         System.out.println("숫자를 입력해주세요 : ");
     }
-    public void FinishGame(){
+    final public void FinishGame(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
     }
-    public void Re_Request(){
+    final public void Re_Request(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
--- src/main/java/baseball/OutInputMessage/UserNumber.java
+++ src/main/java/baseball/OutInputMessage/UserNumber.java
@@ -1,4 +1,23 @@
 package baseball.OutInputMessage;
+import camp.nextstep.edu.missionutils.Console;
 
 public class UserNumber {
+    public String String_Number(){
+        String User_Input_Number = Console.readLine();
+        return User_Input_Number;
+    }
+    public int Change_Number(){
+        String UserInput = String_Number();
+        int Integer_Number;
+        Integer_Number = Integer.parseInt(UserInput);
+        return Integer_Number;
+    }
+    public void Check_Number(){
+        int Final_Number = Change_Number();
+        if(Final_Number < 100 || Final_Number > 1000){
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
+
