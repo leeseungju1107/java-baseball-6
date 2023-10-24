b6e2a36cadb6e16d6649b27617816f352711cf77
seungjoo1 <tmdwn1107@naver.com>
2023-10-24 21:37:19 +0900

    스트라이크, 볼 로직 구현

---
+++ src/main/java/baseball/Control/controller.java
@@ -0,0 +1,9 @@
+package baseball.Control;
+import baseball.OutInputMessage.OutputMessage;
+import baseball.Calculator.calculator;
+
+public class controller {
+    OutputMessage outputmessage = new OutputMessage();
+    calculator Referee = new calculator();
+
+}
